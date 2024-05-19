5ac57ce063b57248a49fdf7518838bd87d8107fc
Maiguelon <miguel_carretero@outlook.com>
2024-05-19 18:36:50 -0300

    Punto 4 terminado

--- tp2_4.c
+++ tp2_4.c
@@ -19,6 +19,7 @@ void generarCompu(compu *compu);
 void mostrarCompus(compu *compus, int cantidad);
 void masAntigua(compu *compus, int cantidad);
 void masVeloz(compu *compus, int cantidad);
+void liberarMemoria(compu *compus, int cantidad);
 
 int main() {
     // preparo el tiempo
@@ -26,18 +27,26 @@ int main() {
 
     // pregunto cuantas compus y reservo la memoria según eso
     int cantidad;
-    printf("Ingrese el número de computadoras (1 a 5): ");
+    printf("Ingrese el numero de computadoras (1 a 5): ");
     scanf("%d", &cantidad);
     compu *computadoras =(compu *)malloc(sizeof(compu) * cantidad);
 
     // Relleno las compus
     compu *ptrCompu = computadoras; // puntero auxiliar
-    for (int i = 0; i < cantidad - 1; i++) {
+    for (int i = 0; i < cantidad; i++) {
         generarCompu(ptrCompu);
         ptrCompu++;
     }
 
-    
+    // Muestro las compus
+    mostrarCompus(computadoras, cantidad);
+    // Muestro la pc más antigua
+    masAntigua(computadoras, cantidad);
+    // Muestro la mas veloz
+    masVeloz(computadoras, cantidad);
+
+    // Libero la memoria
+    liberarMemoria(computadoras, cantidad);
 }
 
 void generarCompu(compu *compu)
@@ -56,7 +65,7 @@ void mostrarCompus(compu *compus, int cantidad)
     compu *ptrCompu = compus;
 
     for (int i = 0; i < cantidad; i++) {
-        printf("Computadora %d: \n", i + 1);
+        printf("\nComputadora %d: \n", i + 1);
         printf("Velocidad: %d\n", ptrCompu->velocidad);
         printf("Anio: %d\n", ptrCompu->anio);
         printf("Cantidad: %d\n", ptrCompu->cantidad);
@@ -75,8 +84,35 @@ void masAntigua(compu *compus, int cantidad)
         if (ptrCompu->anio < masAntiqua->anio) {
             masAntiqua = ptrCompu;
         }
+        ptrCompu++;
     }
 
-    printf("La computadora mas Antiqua es:\n");
+    printf("\nLa computadora mas Antiqua es:\n");
     mostrarCompus(masAntiqua, 1);
+}
+
+void masVeloz(compu *compus, int cantidad)
+{
+    compu *ptrCompu = compus;
+    compu *fastest = compus;
+
+    for (int i = 0; i < cantidad; i++)
+    {
+        if (ptrCompu->velocidad > fastest->velocidad) {
+            fastest = ptrCompu;
+        }
+        ptrCompu++;
+    }
+    
+    printf("\nLa computadora mas Veloz es:\n");
+    mostrarCompus(fastest, 1);
+}
+
+void liberarMemoria(compu *compus, int cantidad)
+{
+    for (int i = 0; i < cantidad; i++)
+    {
+        free(compus[i].tipo_cpu);
+    }
+    free(compus);
 }
